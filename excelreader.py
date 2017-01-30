68831d58a1eb25294c147cb0b1fc61de2c873d2a
345088706 <345088706@qq.com>
2017-01-30 19:19:45 +0800

    添加源码

---
+++ excelreader.py
@@ -0,0 +1,69 @@
+# encoding: utf-8
+from docx import Document
+from docx.shared import Inches
+import sys,json,os,zipfile,xlrd
+reload(sys) 
+sys.setdefaultencoding('utf-8') 
+
+data = xlrd.open_workbook(u'sourcedata.xls')
+table = data.sheet_by_index(0)
+
+def rmdir(top):
+    for root, dirs, files in os.walk(top, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.rmdir(top)
+
+def zip_dir(dirname,zipfilename):
+    filelist = []
+    if os.path.isfile(dirname):
+        filelist.append(dirname)
+    else:
+        for root, dirs, files in os.walk(dirname):
+            for name in files:
+                filelist.append(os.path.join(root, name))
+    zf = zipfile.ZipFile(zipfilename, "w", zipfile.zlib.DEFLATED)
+    for tar in filelist:
+        arcname = tar[len(dirname):]
+        zf.write(tar,arcname)
+    zf.close()
+
+def replace_file(filename, replace_dict):
+    text = None
+    with open(filename) as r:
+        text = r.read()
+        r.close()
+    if text:
+        for (keyword, value) in replace_dict.items():
+            text = text.replace(keyword, value)
+        with open(filename, 'w') as w:
+            w.write(text)
+            w.close()
+
+def replace_with_template(template, docxname, project_no, project, project_en, date, date_en):
+    f = zipfile.ZipFile(template)
+    tmpdir = '.tmp' + docxname
+    f.extractall(tmpdir)
+
+    doc_dict = {
+        '{project_no}': project_no,
+        '{project}': project,
+        '{project_en}': project_en,
+        '{date}': date,
+        '{date_en}': date_en
+    }
+    footer_dict = {
+        '{project_no}': project_no
+    }
+    replace_file(tmpdir + '/word/document.xml', doc_dict)
+    replace_file(tmpdir + '/word/footer1.xml', footer_dict)
+    zip_dir(tmpdir, docxname)
+    rmdir(tmpdir)
+
+for i in range(table.nrows):
+    if i==0:
+        continue
+    col = table.row_values(i)
+    replace_with_template('template.docx', ''.join(['Section ', str(col[0]), '.docx']), col[1], col[2], col[3], col[4], col[5])
